eab86539c49655f843accc6937b5e6d21ecb984c
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-05-31 13:24:09 -0700

    land

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -2,6 +2,20 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ * Return: size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
  * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array
  * @heap: Pointer to the root node of the Heap
  * @size: Address to store the size of the array
