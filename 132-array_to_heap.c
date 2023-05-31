5784272729dbd68a48d8dbb230ef240b19719dbf
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-05-31 18:45:05 +0300

    Update 132-array_to_heap.c

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -3,28 +3,6 @@
 /**
  * array_to_heap - builds a Max Binary Heap tree
  *
-<<<<<<< HEAD
- * Return: Pointer to the root node of the created
- * Binary Heap, or NULL on failure
- */
-heap_t *array_to_heap(int *array, size_t size)
-{
-	heap_t *root = NULL;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		if (heap_insert(&root, array[i]) == NULL)
-		{
-			binary_tree_delete(root);
-			return (NULL);
-		}
-	}
-	return (root);
-=======
  * @array: pointer to the first element of the array
  * @size: number of element in the array
  * Return: pointer to the root node of the AVL tree
@@ -42,5 +20,4 @@ heap_t *array_to_heap(int *array, size_t size)
 	}
 
 	return (tree);
->>>>>>> 489e6b7fc8ce1e6f148cbffb89f113deb8f8f6d2
 }
