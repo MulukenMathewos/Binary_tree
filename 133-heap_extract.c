790b81bfb237626d4dcb931af10adaba42e22e06
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-05-31 18:44:27 +0300

    lop

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -7,46 +7,6 @@
  */
 heap_t *max(heap_t *tree)
 {
-<<<<<<< HEAD
-	int value;
-
-	if (root == NULL || *root == NULL)
-		return (0);
-
-	value = (*root)->n;
-
-	/* If the root is the only node in the heap */
-	if ((*root)->left == NULL && (*root)->right == NULL)
-	{
-		free(*root);
-		*root = NULL;
-		return (value);
-	}
-
-	/* Find the last level-order node to replace the root */
-	heap_t *last_node = get_last_node(*root);
-
-	if (last_node->parent->left == last_node)
-		last_node->parent->left = NULL;
-	else
-		last_node->parent->right = NULL;
-
-	/* Replace the root with the last node */
-	last_node->parent = NULL;
-	last_node->left = (*root)->left;
-	last_node->right = (*root)->right;
-	if ((*root)->left)
-		(*root)->left->parent = last_node;
-	if ((*root)->right)
-		(*root)->right->parent = last_node;
-
-	free(*root);
-	*root = last_node;
-
-	/* Restore the Max Heap property */
-	heapify_down(*root);
-	return (value);
-=======
 	heap_t *curr_max, *left_max, *right_max;
 
 	if (!tree->left)
@@ -65,7 +25,6 @@ heap_t *max(heap_t *tree)
 			curr_max = tree;
 	}
 	return (curr_max);
->>>>>>> 489e6b7fc8ce1e6f148cbffb89f113deb8f8f6d2
 }
 
 /**
@@ -74,22 +33,6 @@ heap_t *max(heap_t *tree)
  */
 void recurse_extract(heap_t *tree)
 {
-<<<<<<< HEAD
-	size_t height = binary_tree_height(root);
-	size_t index;
-
-	/* Find the last level-order node */
-	for (index = 1; index < height; index++)
-	{
-		size_t mask = 1 << (height - index - 1);
-
-		if (root->left && !(index & mask))
-			root = root->left;
-		else if (root->right && (index & mask))
-			root = root->right;
-	}
-	return (root);
-=======
 	heap_t *sub_max, *right_max = NULL;
 
 	if (!tree->left)
@@ -110,7 +53,6 @@ void recurse_extract(heap_t *tree)
 	}
 	else
 		recurse_extract(sub_max);
->>>>>>> 489e6b7fc8ce1e6f148cbffb89f113deb8f8f6d2
 }
 
 /**
@@ -118,39 +60,6 @@ void recurse_extract(heap_t *tree)
  * @root: The pointer to the root of the tree.
  * Return: The value of the extracted node.
  */
-<<<<<<< HEAD
-void heapify_down(heap_t *node)
-{
-	heap_t *max_child;
-
-	while (1)
-	{
-		if (node->left == NULL)
-			break;
-
-		max_child = node->left;
-
-		if (node->right != NULL && node->right->n > node->left->n)
-			max_child = node->right;
-		if (max_child->n <= node->n)
-			break;
-
-		swap_values(&node->n, &max_child->n);
-		node = max_child;
-	}
-}
-
-/**
- * swap_values - Swaps two integer values
- * @a: Pointer to the first value
- * @b: Pointer to the second value
- */
-void swap_values(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-=======
 int heap_extract(heap_t **root)
 {
 	int value;
@@ -167,5 +76,4 @@ int heap_extract(heap_t **root)
 	}
 	recurse_extract(*root);
 	return (value);
->>>>>>> 489e6b7fc8ce1e6f148cbffb89f113deb8f8f6d2
 }
